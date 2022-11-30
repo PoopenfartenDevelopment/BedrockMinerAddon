aeb639a16c60acabb16ff69ca258aecc169b30bc
Gnome <85032707+TrashmanEatGarbage@users.noreply.github.com>
2022-11-29 17:45:08 -0800

    category

--- src/main/java/com/skid/bedrockbreaker/BedrockBreaker.java
+++ src/main/java/com/skid/bedrockbreaker/BedrockBreaker.java
@@ -3,23 +3,17 @@ package com.skid.bedrockbreaker;
 import com.mojang.logging.LogUtils;
 import com.skid.bedrockbreaker.modules.activate;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
 public class BedrockBreaker extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category MAIN = new Category("Main");
 
     @Override
     public void onInitialize() {
         LOG.info("Initializing bedrock breaker");
         Modules.get().add(new activate());
 
-    }
-    @Override
-    public void onRegisterCategories() {
-        Modules.registerCategory(MAIN);
     }
 
     @Override
